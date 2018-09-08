d7a555319da2a733225a1d6b0ca8c0b0aa9c7171
mannycolon <colonmanuel7@gmail.com>
2018-09-08 16:21:29 -0400

    created story generator bot

--- src/storyBot.js
+++ src/storyBot.js
@@ -8,3 +8,38 @@ const bot = new Twit({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000
 });
+
+const grammar = tracery.createGrammar({
+  'character': ['Danny', 'Mark', 'Jhon', 'Aida'],
+  'action': ['walk', 'stroll', 'meander'],
+  'place': ['office', 'bank', 'court'],
+  'object': ['letter', 'paper', 'bride'],
+  'setPronouns': [
+    '[they:they][them:them][their:their][theirs:theirs]',
+    '[they:she][them:her][their:her][theirs:hers]',
+    '[they:he][them:him][their:his][theirs:his]'
+  ],
+  'setJob': [
+    '[job:lawyer][actions:argued in court,filed some paperwork]',
+    '[job:inspector][actions:talked with the lawyer,conducted meetings]',
+    '[job:officer][actions:arrested people,stood in the courtroom]'
+  ], // will join two variables together
+  'story': ['#protagonist# the #job# went to the #place# every day. Usually #they# #actions#. Then #they# picked up #their# #object#.'],
+  'origin': ['#[#setPronouns#][#setJob#][protagonist:#character#]story#']
+})
+/**
+ * Example of origin
+ *   'origin': ['#character.capitalize# #action.ed# to the #place# for #object.a# or to get some #object.s#.']
+ */
+
+grammar.addModifiers(tracery.baseEngModifiers)
+
+const story = grammar.flatten('#origin#')
+
+console.log(story)
+
+
+bot.post('statuses/update', { status: story }, (err, data, response) => {
+  if (err) console.log(err)
+  else console.log('Bot has Tweeted ' + story)
+})
