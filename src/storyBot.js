db539a8c3a1abaacc7d4183beea58a35ef54036f
mannycolon <colonmanuel7@gmail.com>
2018-09-08 15:14:19 -0400

    removed keyfile.json

---
+++ src/storyBot.js
@@ -0,0 +1,10 @@
+const Twit = require('twit')
+const tracery = require('tracery-grammar')
+
+const bot = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000
+});
