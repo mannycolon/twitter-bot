01f05541d93fc90c6a5b355b3b166b5ee1a268c3
mannycolon <colonmanuel7@gmail.com>
2018-09-11 18:35:00 -0400

    added readme file and moved audio bot to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-const Twit = require('twit')
-const request = require('request')
-const fs = require('fs-extra')
 
-const bot = new Twit({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000
-});
+const audioBot = require('./audioBot')
